9f35ab2e39a9599bc79ac74daff468417674dec9
YellowHan <89592727+YellowHan@users.noreply.github.com>
2022-11-10 14:43:34 +0900

    Add files via upload

--- src/assembly/Assembly.java
+++ src/assembly/Assembly.java
@@ -1,16 +1,11 @@
 package assembly;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+public class Assembly { // 조립 클래스
+	
+	public Assembly() {
+		System.out.println("Assembly::Default Constructor invoked.");
+	} // Default Constructor
+	
+	
 
-public class Assembly {
-    public static void main(String[] args) {
-        List<Object> list = new ArrayList<>();
-        list.add(100);
-        list.add("100");
-        list.add("가나다라");
-        System.out.println(list);
-        System.out.println(list.get(0));
-    }
 } // end class
